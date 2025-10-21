06280b63896bdd5f3f618fd632636d10aeb3c153
drkae456 <brycedbacon@gmail.com>
2025-10-21 11:48:05 +1100

    fixed build issues

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ fi
 echo "Converting build for Cloudflare Pages..."
 # Now run OpenNext build to convert for Cloudflare using local installation
 # Use local node_modules to ensure wrangler is available
-npx @opennextjs/cloudflare build
+./node_modules/.bin/opennextjs-cloudflare build
 
 echo "Fixing worker structure for Cloudflare Pages..."
 chmod +x fix-worker.sh
